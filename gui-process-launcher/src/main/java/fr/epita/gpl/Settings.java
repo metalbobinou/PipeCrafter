013b8cfc4b6d23142a291ca7c37b7a34cb092bad
ivanceA <iv.amovin@icloud.com>
2023-11-07 18:02:02 +0100

    Settings: added guards for setters

--- gui-process-launcher/src/main/java/fr/epita/gpl/Settings.java
+++ gui-process-launcher/src/main/java/fr/epita/gpl/Settings.java
@@ -24,11 +24,19 @@ public class Settings {
     }
 
     public static void setExecutionDirectory(File executionDirectory) {
+        if (executionDirectory == null | !executionDirectory.isDirectory() | !executionDirectory.canWrite()) {
+            throw new IllegalArgumentException(
+                    "The specified execution directory is not specified, is not a directory or is not writable.");
+        }
         Settings.executionDirectory = executionDirectory;
     }
 
     public static void setExecutionDirectory(String executionDirectoryPath) {
         Settings.executionDirectory = new File(executionDirectoryPath);
+        if (executionDirectory == null | !executionDirectory.isDirectory() | !executionDirectory.canWrite()) {
+            throw new IllegalArgumentException(
+                    "The specified execution directory is not specified, is not a directory or is not writable.");
+        }
     }
 
     public static File getOutputSavingDirectory() {
@@ -36,11 +44,18 @@ public class Settings {
     }
 
     public static void setOutputSavingDirectory(File outputSavingDirectory) {
+        if (outputSavingDirectory == null | !outputSavingDirectory.isDirectory() | !outputSavingDirectory.canWrite()) {
+            throw new IllegalArgumentException(
+                    "The specified saving directory is not specified, is not a directory or is not writable.");
+        }
         Settings.outputSavingDirectory = outputSavingDirectory;
     }
 
     public static void setOutputSavingDirectory(String outputSavingDirectoryPath) {
         Settings.outputSavingDirectory = new File(outputSavingDirectoryPath);
+        if (outputSavingDirectory == null | !outputSavingDirectory.isDirectory() | !outputSavingDirectory.canWrite()) {
+            throw new IllegalArgumentException(
+                    "The specified saving directory is not specified, is not a directory or is not writable.");
+        }
     }
-
 }
