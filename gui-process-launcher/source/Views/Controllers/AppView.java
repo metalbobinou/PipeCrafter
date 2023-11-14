7545ef045d08618f36cbd3926499d88f9263d335
ivanceA <iv.amovin@icloud.com>
2023-11-14 18:05:34 +0100

    Made AppView entry point

--- gui-process-launcher/source/Views/Controllers/AppView.java
+++ gui-process-launcher/source/Views/Controllers/AppView.java
@@ -1,5 +1,29 @@
 package Views.Controllers;
 
-public class AppView {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 
+import java.io.IOException;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+
+public class AppView extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        Parent root = FXMLLoader.load(getClass().getResource("test.fxml"));
+        stage.setTitle("Execution Pipeline Builder");
+        stage.setScene(new Scene(root));
+        // stage.sizeToScene();
+        stage.show();
+        stage.setMinWidth(stage.getMinWidth()); // set to "main" anchor pane min sizes
+        stage.setMinHeight(stage.getMinHeight());
+    }
+
+    public static void main(String[] args) {
+        // V0.demo();
+        launch();
+    }
 }
