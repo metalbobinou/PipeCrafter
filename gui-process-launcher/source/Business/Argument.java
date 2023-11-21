ddcf4acbab005ce479898f63cbef16548ece2074
ivanceA <iv.amovin@icloud.com>
2023-11-21 18:07:20 +0100

    Business.Argument: filled

--- gui-process-launcher/source/Business/Argument.java
+++ gui-process-launcher/source/Business/Argument.java
@@ -1,5 +1,31 @@
 package Business;
 
+/** Business class for arguments */
 public class Argument {
+
+    // region Attributes
+
+    /** Used to save the argument being added, null when un/reset */
+    private static Models.Argument addedArg = null;
+
+    // endregion
+
+    // region Methods
+
+    // endregion
+
+    // region Getters and Setters
+
+    public static Models.Argument getAddedArg() {
+        Models.Argument res = addedArg;
+        addedArg = null;
+        return res;
+    }
+
+    public static void setAddedArg(Models.Argument addedArg) {
+        Argument.addedArg = addedArg;
+    }
+
+    // endregion
 
 }
