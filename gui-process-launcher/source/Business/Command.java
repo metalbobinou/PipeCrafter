a1ddd141757999d49ff10dc43305186e40b4ca7e
ivanceA <iv.amovin@icloud.com>
2023-11-21 14:28:33 +0100

    Business.Command: filled

--- gui-process-launcher/source/Business/Command.java
+++ gui-process-launcher/source/Business/Command.java
@@ -1,5 +1,33 @@
 package Business;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.scene.Node;
+
+/** Business class for commands */
 public class Command {
 
+    // region Attributes
+
+    /** List of all commands */
+    private static List<Models.Command> commands = new ArrayList<>();
+
+    // endregion
+
+    // region Methods
+
+    /**
+     * Add a command to the list and set up its controller
+     * 
+     * @param controller controller for which to create a command model
+     * @param node       FX object representing the command
+     */
+    public static void addCommand(Views.Command controller, Node node) {
+        Models.Command model = new Models.Command(commands.size() + 1);
+        controller.SetUp(node, model);
+        commands.add(model);
+    }
+
+    // endregion
 }
