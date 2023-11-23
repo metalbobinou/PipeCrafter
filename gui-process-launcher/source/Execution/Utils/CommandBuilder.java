e1c3cb5e8b571a5f45cb1286d1d318dd65185d1a
ivanceA <iv.amovin@icloud.com>
2023-11-23 17:14:37 +0100

    Execution.CommandBuilder: updated for V1 + set permissions for used outputs

--- gui-process-launcher/source/Execution/Utils/CommandBuilder.java
+++ gui-process-launcher/source/Execution/Utils/CommandBuilder.java
@@ -1,11 +1,14 @@
 package Execution.Utils;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
 import Business.Settings.Shell;
-import Utils.OutputStream;
+import Models.Argument.Type;
 
 /** Handle command building operations */
 public class CommandBuilder {
@@ -13,34 +16,39 @@ public class CommandBuilder {
     // region Methods
 
     /**
-     * Build and return the path to a saved output
-     * 
-     * @param step   step for which to retreive the output
-     * @param stream type of the saved stream to gather
-     * @return string representation of the path
-     */
-    public static String getPathForOutputOfStep(int step, OutputStream stream) {
-        return Paths.get(Business.Settings.getOutputSavingDirectory().toString(), step + stream.getExtension())
-                .toString();
-    }
-
-    /**
      * Process the given command to be able to use it with the choosen shell
      * 
-     * @param command the command as a list of strings
-     * @return the (wrapped) command as a list of strings
+     * @param cmdList the command as a list of strings
+     * @return the command as a list of strings
      */
-    public static List<String> getCommand(List<String> command) {
+    public static List<String> getCommand(Models.Command command) {
+
+        List<String> cmdList = new ArrayList<>();
 
         if (Business.Settings.getUsedShell() == Shell.NONE) {
-            return command;
+
+            cmdList.add(command.getCmd());
+            for (Models.Argument arg : command.getArgumentList()) {
+                String argAsString = arg.toString();
+
+                if (arg.getType() == Type.OUTPUT) {
+                    setPermissions(argAsString);
+                }
+                cmdList.add(argAsString);
+            }
+            return cmdList;
         }
 
         // - Wrap the command to use it with a shell
         StringBuilder commandBuilder = new StringBuilder();
         // Turn the list into a single string
-        for (String string : command) {
-            commandBuilder.append(" ").append(string);
+        for (Models.Argument arg : command.getArgumentList()) {
+            String argAsString = arg.toString();
+
+            if (arg.getType() == Type.OUTPUT) {
+                setPermissions(argAsString);
+            }
+            commandBuilder.append(" ").append(argAsString);
         }
         commandBuilder.deleteCharAt(0);
         String commandString = commandBuilder.toString();
@@ -55,12 +63,24 @@ public class CommandBuilder {
             case WSL:
                 return Arrays.asList("wsl", commandString);
             default:
-                return command;
+                return cmdList;
         }
     }
 
-    // Add function to check used output files have exec rights (read/write is given
-    // when generated)
+    /**
+     * Give 755 permissions to a file
+     * 
+     * @param pathAsString path to the file to change permissions for
+     */
+    private static void setPermissions(String pathAsString) {
+        Path path = Paths.get(pathAsString);
+        try {
+            Files.setPosixFilePermissions(path,
+                    PosixFilePermissions.fromString("rwxr-xr-x"));
+        } catch (Exception e) {
+            System.err.println("Could not set permissions for file " + pathAsString);
+        }
+    }
 
     // endregion
 }
