3294ab5bbdfbb07f6dc6a3790564695ffd4e991f
ivanceA <iv.amovin@icloud.com>
2023-11-23 17:08:31 +0100

    Business.Command: added execution

--- gui-process-launcher/source/Business/Command.java
+++ gui-process-launcher/source/Business/Command.java
@@ -32,5 +32,46 @@ public class Command {
         commands.add(model);
     }
 
+    /**
+     * Interprate the the appropriate action to trigger when the "run" button
+     * of a command is pressed based on the app and command's context
+     * 
+     * @param command command from which the "run" button was clicked
+     */
+    public static void decideExec(Models.Command command) {
+        if (Business.App.isExecuting()) {
+            // TODO send prompt "Step X is being executed. Proceeding will abort it. Do you
+            // want to continue?". If command.getState() == RUNNING then return (stop
+            // called)
+        }
+
+        // If continue:
+        switch (command.getState()) {
+            case ALREADY_RUN:
+                // TODO restart prompt (ask if needed) then call run
+                break;
+            case NEXT_TO_RUN:
+                run(command);
+                break;
+            case RUNNING:
+                // error
+                break;
+            case TO_RUN:
+                // TODO check if dependancies okay
+                break;
+        }
+    }
+
+    /**
+     * Launch the execution of a command
+     * 
+     * @param command the command to launch execution for
+     */
+    private static void run(Models.Command command) {
+        command.setState(State.RUNNING);
+        Business.App.setRun(command);
+        Execution.ProcessManager.execute(command);
+    }
+
     // endregion
 }
