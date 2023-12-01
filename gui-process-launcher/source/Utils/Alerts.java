810e8290306f8a2fb151251227077d256c12c6b1
ivanceA <iv.amovin@icloud.com>
2023-12-01 14:23:10 +0100

    Utils.Alerts: added alert for reading output file

--- gui-process-launcher/source/Utils/Alerts.java
+++ gui-process-launcher/source/Utils/Alerts.java
@@ -8,16 +8,24 @@ public class Alerts {
 
     // region Attributes
 
+    /** Alert for when the maximum number of characeters has been reached */
     private static Alert maxCharAlert;
+
+    private static Alert failedReadingOutputFileAlert;
 
     // endregion
 
     // region Methods
 
+    /** Initialize all used alerts */
     public static void init() {
         maxCharAlert = new Alert(AlertType.WARNING);
         maxCharAlert.setTitle("Warning");
         maxCharAlert.setContentText("Character limit reached");
+
+        failedReadingOutputFileAlert = new Alert(AlertType.ERROR);
+        failedReadingOutputFileAlert.setTitle("Error");
+        failedReadingOutputFileAlert.setContentText("Error reading output file");
     }
 
     // endregion
@@ -30,6 +38,14 @@ public class Alerts {
 
     public static void setMaxCharAlert(Alert maxCharAlert) {
         Alerts.maxCharAlert = maxCharAlert;
+    }
+
+    public static Alert getFailedReadingOutputFileAlert() {
+        return failedReadingOutputFileAlert;
+    }
+
+    public static void setFailedReadingOutputFileAlert(Alert failedReadingFileAlert) {
+        Alerts.failedReadingOutputFileAlert = failedReadingFileAlert;
     }
 
     // endregion
