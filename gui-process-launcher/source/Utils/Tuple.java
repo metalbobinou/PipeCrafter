be9e2eddf2e6a40ddf22c47b86835cc541d7b736
ivanceA <iv.amovin@icloud.com>
2023-12-13 19:37:51 +0100

    Utils.Tuple: added

---
+++ gui-process-launcher/source/Utils/Tuple.java
@@ -0,0 +1,25 @@
+package Utils;
+
+/** A tuple class */
+public class Tuple<A, B> {
+
+    // region Attributes
+
+    /** The first element of the tuple */
+    public A first;
+
+    /** The second element of the tuple */
+    public B second;
+
+    // endregion
+
+    // region Constructor
+
+    public Tuple(A first, B second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    // endregion
+
+}
