51bac5e36aaa807eb2054c09e6627b76986f39cc
ivanceA <iv.amovin@icloud.com>
2023-12-22 16:12:59 +0100

    Test.Utils.Ids: created

---
+++ gui-process-launcher/test/Utils/Ids.java
@@ -0,0 +1,18 @@
+package Utils;
+
+/** Class containing all ids used throughout the test/page classes */
+public class Ids {
+    // #region Settings
+
+    /** The title displayed on the settings page */
+    public final static String SETTINGS_TITLE_LABEL = "#settingsLabel";
+
+    // #endregion
+
+    // #region Main screen
+
+    /** The button used to open the settings page */
+    public final static String SETTINGS_BUTTON_ID = "#settingsButton";
+
+    // #endregion
+}
