142385f4735451cd3f7a93a36469b6bbf0dd4629
ivanceA <iv.amovin@icloud.com>
2024-01-03 16:08:10 +0100

    Settings: added ids

--- gui-process-launcher/test/Utils/Ids.java
+++ gui-process-launcher/test/Utils/Ids.java
@@ -4,14 +4,20 @@ package Utils;
 public class Ids {
     // #region Settings
 
+    /** Back button */
+    public final static String SETTINGS_BACK_BUTTON_ID = "#backButton";
+
     /** The title displayed on the settings page */
-    public final static String SETTINGS_TITLE_LABEL = "#settingsLabel";
+    public final static String SETTINGS_TITLE_LABEL_ID = "#settingsLabel";
 
     /** TextField for the execution folder */
-    public final static String EXEC_FOLDER_FIELD = "#execFolderField";
+    public final static String EXEC_FOLDER_FIELD_ID = "#execFolderField";
 
     /** TextField for the output saving folder */
-    public final static String OUTPUT_FOLDER_FIELD = "#outputFolderField";
+    public final static String OUTPUT_FOLDER_FIELD_ID = "#outputFolderField";
+
+    /** ChoiceBox to select the shell to use */
+    public final static String SHELL_CHOICE_BOX_ID = "#shellBox";
 
     // #endregion
 
