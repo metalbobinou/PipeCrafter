64099b8a718b6b19dfeb43aa29a5a96057fd805f
ivanceA <iv.amovin@icloud.com>
2024-01-03 11:47:12 +0100

    Tests.Utils.SuperPage: added pressAndRelease and replaceTextIn methods and platform agnostic ctrl key

--- gui-process-launcher/test/Utils/SuperPage.java
+++ gui-process-launcher/test/Utils/SuperPage.java
@@ -1,11 +1,19 @@
 package Utils;
 
+import javafx.scene.input.KeyCode;
+
 /** Parent class for all page classes */
 public abstract class SuperPage {
 
     // #region Attributes
 
-    private final TestFXBase drive;
+    /** The drive to use to interact with the page */
+    protected final TestFXBase drive;
+
+    /** KeyCode to use to perform actions such as select all, copy and paste */
+    protected final static KeyCode controlKey = System.getProperty("os.name").toLowerCase().contains("mac")
+            ? KeyCode.COMMAND
+            : KeyCode.CONTROL;
 
     // #endregion
 
@@ -13,6 +21,40 @@ public abstract class SuperPage {
 
     public SuperPage(TestFXBase drive) {
         this.drive = drive;
+    }
+
+    // #endregion
+
+    // #region Methods
+
+    /**
+     * Helper method to press and immediately release the given keys
+     * 
+     * @param keys to press and release
+     */
+    private void pressAndRelease(KeyCode... keys) {
+        for (KeyCode key : keys) {
+            drive.press(key);
+        }
+        for (KeyCode key : keys) {
+            drive.release(key);
+        }
+    }
+
+    /**
+     * Replace the text in a writable element
+     * 
+     * @param elementId id of the element to set text for
+     * @param text      text to write
+     */
+    public void replaceTextIn(String elementId, String text) {
+        drive.clickOn(elementId);
+
+        pressAndRelease(controlKey, KeyCode.A);
+
+        drive.write(text);
+
+        pressAndRelease(KeyCode.ENTER);
     }
 
     // #endregion
