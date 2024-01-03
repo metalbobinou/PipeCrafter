f5baad9e148ef7aeaa87a421ed5b8ff4885a1d69
ivanceA <iv.amovin@icloud.com>
2024-01-03 16:10:23 +0100

    Tests.SettingsTests: filled

--- gui-process-launcher/test/SettingsTests/SettingsTests.java
+++ gui-process-launcher/test/SettingsTests/SettingsTests.java
@@ -1,37 +1,105 @@
 package SettingsTests;
 
 import org.junit.jupiter.api.Test;
-import static org.testfx.api.FxAssert.verifyThat;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import Utils.Ids;
+import Business.Settings.Shell;
 import Utils.SettingsPage;
 import Utils.TestFXBase;
-import javafx.scene.control.TextField;
 
 /** Settings related tests class */
 public class SettingsTests extends TestFXBase {
 
-    // #region Tests
+    // #region Test UI shows current settings
 
     @Test
     public void checkOutputDirectory() {
-        new SettingsPage(this);
-        verifyThat(Ids.OUTPUT_FOLDER_FIELD, (TextField tf) -> {
-            return tf.getText().equals(Business.Settings.getOutputSavingDirectory().getAbsolutePath());
-        });
+        new SettingsPage(this).verifyOutputFolderField(null);
     }
 
     @Test
     public void checkExecutionDirectory() {
-        new SettingsPage(this);
-        verifyThat(Ids.EXEC_FOLDER_FIELD, (TextField tf) -> {
-            return tf.getText().equals(Business.Settings.getExecutionDirectory().getAbsolutePath());
-        });
+        new SettingsPage(this).verifyExecFolderField(null);
+
+    }
+
+    @Test
+    public void checkUsedShell() {
+        new SettingsPage(this).verifyUsedShellBox(null);
     }
 
     // #endregion
 
-    // #region Providers
+    // #region Test UI update current settings and show update
+
+    @Test
+    public void writeOutputDir() {
+        SettingsPage page = new SettingsPage(this);
+
+        String newPath = baseURL + "test";
+
+        page.setOutputFolderField(newPath);
+
+        page.verifyOutputFolderField(newPath);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "", " ", "\\", "qwerty", "nwendfo3928y9{}{{@#(*())}}" })
+    public void writeGarbageOutputDir(String text) {
+        SettingsPage page = new SettingsPage(this);
+
+        String currentSetPath = Business.Settings.getOutputSavingDirectory().getAbsolutePath();
+
+        page.setOutputFolderField(text);
+
+        page.verifyErrorAlertDisplayed();
+
+        // check that the path was not changed
+        page.verifyOutputFolderField(currentSetPath);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "", " ", "\\", "qwerty", "nwendfo3928y9{}{{@#(*())}}" })
+    public void writeGarbageExecDir(String text) {
+        SettingsPage page = new SettingsPage(this);
+
+        String currentSetPath = Business.Settings.getExecutionDirectory().getAbsolutePath();
+
+        page.setExecFolderField(text);
+
+        page.verifyErrorAlertDisplayed();
+
+        // check that the path was not changed
+        page.verifyExecFolderField(currentSetPath);
+    }
+
+    @Test
+    public void writeExecDir() {
+        SettingsPage page = new SettingsPage(this);
+
+        String newPath = baseURL + "test";
+
+        page.setExecFolderField(newPath);
+
+        page.verifyExecFolderField(newPath);
+    }
+
+    @ParameterizedTest
+    @EnumSource(Shell.class)
+    public void setShell(Shell shellToUse) {
+        SettingsPage page = new SettingsPage(this);
+
+        page.selectShell(shellToUse);
+
+        page.verifyUsedShellBox(shellToUse);
+    }
+
+    @Test
+    public void goBackToMenu() {
+        new SettingsPage(this).returnToMenu();
+    }
 
     // #endregion
 }
