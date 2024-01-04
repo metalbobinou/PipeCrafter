9e9ce87babfb1b1de5d53bf8fd7fe50d8f6c07f3
ivanceA <iv.amovin@icloud.com>
2024-01-04 09:48:43 +0100

    Added ids

--- gui-process-launcher/test/Utils/Ids.java
+++ gui-process-launcher/test/Utils/Ids.java
@@ -38,8 +38,78 @@ public class Ids {
     /** The text label displaying the status */
     public final static String STATUS_LABEL_ID = "#status_text";
 
+    /** The scroll pane containing the command */
+    public final static String CMD_SCROLLPANE_ID = "#cmdScrollPane";
+
     /** The button used to add new commands */
     public final static String ADD_CMD_BUTTON_ID = "#addCmdButton";
+
+    // #endregion
+
+    // #region Argument type selector
+
+    /** The button (group) to choose a file argument */
+    public final static String FILE_GROUP_ID = "#fileGroup";
+
+    /** The button (group) to choose a text argument */
+    public final static String TEXT_GROUP_ID = "#textGroup";
+
+    /** The button (group) to choose an output argument */
+    public final static String OUTPUT_GROUP_ID = "#outputGroup";
+
+    // #endregion
+
+    // #region
+
+    /** The label displaying an argument's value */
+    public final static String ARGUMENT_VALUE_ID = "#text";
+
+    /** The button to delete an argument */
+    public final static String DELETE_ARGUMENT_BUTTON_ID = "#deleteArgButton";
+
+    // #endregion
+
+    // #region Command
+
+    /** The text field for a command's name */
+    public final static String CMD_NAME_TEXTFIELD_ID = "#nameField";
+
+    /** A command's run button */
+    public final static String CMD_RUN_BUTTON_ID = "#run_button";
+
+    /** A command's delete button */
+    public final static String CMD_DELETE_BUTTON_ID = "#deleteButton";
+
+    /** A command's text field */
+    public final static String CMD_TEXTFIELD_ID = "#textField";
+
+    /** A command's argument scroll pane */
+    public final static String CMD_ARG_SCROLLPANE_ID = "#arguments_scrollPane";
+
+    /** A command's add argument button */
+    public final static String CMD_ADD_ARG_BUTTON_ID = "#addArgButton";
+
+    // #endregion
+
+    // #region Output selector
+
+    /** Command selector box */
+    public final static String CMD_SELECTOR_BOX_ID = "#commandBox";
+
+    /** Stream selector box */
+    public final static String STREAM_SELECTOR_BOX_ID = "#streamBox";
+
+    /** Format selector box */
+    public final static String FORMAT_SELECTOR_BOX_ID = "#formatBox";
+
+    /** Display command checkbox */
+    public final static String DISP_CMD_CHECKBOX_ID = "#displayCheckbox";
+
+    /** Displayed command text */
+    public final static String DISP_CMD_TEXT_ID = "#cmdText";
+
+    /** Done button */
+    public final static String DONE_BUTTON_ID = "#doneButton";
 
     // #endregion
 }
