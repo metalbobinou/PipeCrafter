2adcc3fc58f2b2af4311b61c399d36d744ab9e27
ivanceA <iv.amovin@icloud.com>
2024-01-05 17:30:25 +0100

    comment

--- gui-process-launcher/test/EditTests/ArgEditTests.java
+++ gui-process-launcher/test/EditTests/ArgEditTests.java
@@ -1,6 +1,5 @@
 package EditTests;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
